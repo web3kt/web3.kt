28bd73b40e36088a85a527fe82e6405d2b1f3f32
Junsung Cho <junsung@directional.net>
2024-07-15 14:35:09 +0900

    build(build.gradle.kts): add publishing config

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,17 @@
 plugins {
+    id("maven-publish")
+    id("signing")
     kotlin("jvm")
     kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "org.web3kt"
 version = "0.0.1"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
@@ -24,6 +31,50 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.web3kt"
+            artifactId = "core"
+
+            from(components["java"])
+
+            pom {
+                name.set("Web3.kt")
+                description.set("Web3.kt is a Kotlin implementation of the Besu API.")
+                url.set("https://github.com/web3kt/web3.kt")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/web3kt/web3.kt/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("junsung")
+                        name.set("Junsung Cho")
+                        email.set("junsung.dev@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/web3kt/web3.kt.git")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
